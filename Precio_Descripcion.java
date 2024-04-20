491b435f540686d072d5a58c0d6073f08b33e15a
GaboIraheta <00021223@uca.edu.sv>
2024-04-20 12:15:43 -0600

    :white_check_mark: Agregando nuevamente archivos de proyecto PrimerParcialPOO

--- Precio_Descripcion.java
+++ Precio_Descripcion.java
@@ -1,4 +1,6 @@
+import java.util.Scanner;
+
 public interface Precio_Descripcion {
-    double obtener_precio();
+    double obtener_precio(double precio, Scanner input);
     void mostrar_descripcion();
 }
